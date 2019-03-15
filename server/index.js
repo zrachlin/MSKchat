6fb79c54a2132b900d58687644dc536362ddc471
Zach Rachlin <zachrach@gmail.com>
2019-03-14 22:41:39 -0400

    added passport,express-session, connect-session-sequelize, and compression to package.json and built out server/index.js

---
+++ server/index.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const db = require('./db');
+const compression = require('compression');
+const session = require('express-session');
+const passport = require('passport');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({ db });
+const PORT = process.env.PORT || 8080;
+const app = express();
+const socketio = require('socket.io');
+module.exports = app;
+
+// Global Mocha hook, used for resource cleanup.
+// Otherwise, Mocha v4+ never quits after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close the session store', () => sessionStore.stopExpiringSessions());
+}
+
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'));
+
+  // static middleware
+  app.use(express.static(path.join(__dirname, '..', 'node_modules')));
+  app.use(express.static(path.join(__dirname, '..', 'public')));
+
+  // body parsing middleware
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // compression middleware
+  app.use(compression());
+
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // AUTH routes
+  app.use('/auth', require('./auth'));
+
+  // API routes
+  app.use('/api', require('./api'));
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found');
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
+
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  });
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+};
+
+const syncDb = () => db.sync().then(() => console.log('Database is synced'));
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`MSKchat server is open on port ${PORT}`)
+  );
+
+  // set up our socket control center
+  const io = socketio(server);
+  require('./socket')(io);
+};
+
+async function bootApp() {
+  await sessionStore.sync();
+  await syncDb();
+  await createApp();
+  await startListening();
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp();
+} else {
+  createApp();
+}
