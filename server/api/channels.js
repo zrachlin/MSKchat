d5454d59bc0bc0a5cc44360190ee6b5a4c41f449
Zach Rachlin <zachrach@gmail.com>
2019-03-14 22:01:11 -0400

    fleshed out api routes for messages and channels

---
+++ server/api/channels.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const { Channel, Message } = require('../db/models');
+
+module.exports = router;
+
+// GET /api/channels
+router.get('/', async (req, res, next) => {
+  try {
+    const channels = await Channel.findAll();
+    res.json(channels);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST /api/channels
+router.post('/', async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const channel = await Channel.create({ name });
+    res.json(channel);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Currently Unused By Frontend:
+
+// GET /api/channels/:channelId
+router.get('/:channelId', async (req, res, next) => {
+  try {
+    const channel = await Channel.findById(req.params.channelId);
+    res.json(channel);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /api/channels/:channelId/messages
+router.get('/:channelId/messages', async (req, res, next) => {
+  try {
+    const channelId = req.params.channelId;
+    const messages = await Message.findAll({ where: { channelId } });
+    res.json(messages);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE /api/channels
+router.delete('/:channelId', async (req, res, next) => {
+  try {
+    const id = req.params.channelId;
+    await Channel.destroy({ where: { id } });
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
---
+++ server/api/messages.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Message } = require('../db/models');
+
+module.exports = router;
+
+// GET /api/messages
+router.get('/', async (req, res, next) => {
+  try {
+    const messages = await Message.findAll();
+    res.json(messages);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST /api/messages
+router.post('/', async (req, res, next) => {
+  try {
+    const { content } = req.body;
+    const message = await Message.create({ content, userId: req.user.id });
+    res.json(message);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Currently unused by frontend:
+
+// PUT /api/messages
+router.put('/:messageId', async (req, res, next) => {
+  try {
+    const messageId = req.params.messageId;
+    const message = await Message.findById(messageId);
+    await message.update(req.body);
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE /api/messages
+router.delete('/:messageId', async (req, res, next) => {
+  try {
+    const id = req.params.messageId;
+    await Message.destroy({ where: { id } });
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+});
