e04d76dd229df8bd9c294daf5383fbf222ae05b9
Zach Rachlin <zachrach@gmail.com>
2019-03-17 23:25:52 -0400

    added questions to seed file

--- seed.js
+++ seed.js
@@ -10,6 +10,9 @@ const seed = async () => {
     User.create({ username: 'user2', password: '234' }),
     User.create({ username: 'user3', password: '345' }),
   ]);
+
+  const zr = await User.create({ username: 'zrachlin', password: 'zr' });
+  users.push(zr);
   console.log(`Seeded ${users.length} users.`);
 
   const channels = await Promise.all([
@@ -17,11 +20,12 @@ const seed = async () => {
     Channel.create({ name: 'jobs' }),
     Channel.create({ name: 'random' }),
     Channel.create({ name: 'machine-learning' }),
+    Channel.create({ name: 'questions' }),
   ]);
   console.log(`Seeded ${channels.length} channels.`);
-
   const general = await Channel.findOne({ where: { name: 'general' } });
   const jobs = await Channel.findOne({ where: { name: 'jobs' } });
+  const questions = await Channel.findOne({ where: { name: 'questions' } });
 
   const messages = await Promise.all([
     Message.create({ userId: 1, content: 'Hi there', channelId: general.id }),
@@ -30,10 +34,39 @@ const seed = async () => {
       content: 'Good Morning!',
       channelId: general.id,
     }),
+    Message.create({
+      userId: zr.id,
+      content: 'What brought you to MSK and this team specifically?',
+      channelId: questions.id,
+    }),
+    Message.create({
+      userId: zr.id,
+      content: 'What is the biggest challenge your team is facing?',
+      channelId: questions.id,
+    }),
+    Message.create({
+      userId: zr.id,
+      content: 'What are you primarily looking for in a new team member?',
+      channelId: questions.id,
+    }),
+    Message.create({
+      userId: zr.id,
+      content:
+        'How can I best get up to speed with the bioinformatics concepts and terminology? Best resources?',
+      channelId: questions.id,
+    }),
+    Message.create({
+      userId: zr.id,
+      content:
+        "What is your favorite part about working at MSK? What's the most frustrating part?",
+      channelId: questions.id,
+    }),
   ]);
+  // order matters, so do these sequentially
   const m1 = await Message.create({
     userId: 3,
-    content: 'I need to hire someone to do full-stack engineering ASAP',
+    content:
+      'I need to hire someone to do full-stack software development for the DHI ASAP',
     channelId: jobs.id,
   });
   const m2 = await Message.create({
