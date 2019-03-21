edc0af665b45a69ab6c7ae3eb71c5cdd7c1a1d1d
Zach Rachlin <zachrach@gmail.com>
2019-03-20 22:50:28 -0400

    added comments describing connection string changes

--- server/db/db.js
+++ server/db/db.js
@@ -4,5 +4,14 @@ const db = new Sequelize('postgres://localhost/mskchat', {
 });
 module.exports = db;
 
-//postgres://user:user@127.0.0.1:5434/project
-//localhost:5432/mskchat'
+// Issues with connection string:
+// Reference issue: https://github.com/sequelize/sequelize/issues/4165
+
+// Formerly:
+// 'postgres://localhost:5434/mskchat'
+
+// Changed to:
+// 'postgres://localhost/mskchat'
+
+// Other potential option:
+// 'postgres://user:user@127.0.0.1:/project'
