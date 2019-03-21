71c6cd553fa7fe54b24081fa866de4f44956bbda
Zach Rachlin <zachrach@gmail.com>
2019-03-20 22:19:40 -0400

    removed port number from connection string

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(
-  'postgres://localhost:5432/mskchat',
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize('postgres://localhost/mskchat', {
+  logging: false,
+});
 module.exports = db;
+
+//postgres://user:user@127.0.0.1:5434/project
+//localhost:5432/mskchat'
